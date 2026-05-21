ea0c7f70c653699ed2388c7f62ba8e3f9a16b1be
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-05-21 21:03:40 +0200

    add 100 random colored snail

--- Program.cs
+++ Program.cs
@@ -13,3 +13,12 @@ string name = "Peter";
 
 Console.WriteLine($"Hello {name}, my favofir veggie is {vegetables[Random.Shared.Next(vegetables.Length)]}");
 
+for (int i = 0; i < 100; i++)
+{
+    Console.SetCursorPosition(
+        Random.Shared.Next(Console.WindowWidth - 1),
+        Random.Shared.Next(Console.WindowHeight - 1));
+
+    Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
+    Console.WriteLine("_._@/\"");
+}
