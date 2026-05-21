1e39c833d97ca5f0da5169b9d0f3ed092febabb8
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-05-21 20:38:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] vegetables = [
+    "potato",
+    "paprika",
+    "carrot",
+    ];
+
+string name = "Zoltan";
+
+Console.WriteLine($"Hello {name}, my favofir veggie is {vegetables[Random.Shared.Next(vegetables.Length)]}");
