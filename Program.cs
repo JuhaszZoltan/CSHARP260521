81c7358902dd18c402bc257ac59ebe7dd606d1c2
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-05-21 21:06:06 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -16,9 +16,10 @@ Console.WriteLine($"Hello {name}, my favofir veggie is {vegetables[Random.Shared
 for (int i = 0; i < 100; i++)
 {
     Console.SetCursorPosition(
-        Random.Shared.Next(Console.WindowWidth - 1),
+        Random.Shared.Next(Console.WindowWidth - 6),
         Random.Shared.Next(Console.WindowHeight - 1));
-
     Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
-    Console.WriteLine("_._@/\"");
+    Console.WriteLine("._@/\"");
 }
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
