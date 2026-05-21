1a38b4d375c6582b3ed3321b81a3bf969e3714a9
BitMiller <gp.bitmiller@gmail.com>
2026-05-21 21:03:33 +0200

    @260521-4-2103: Vedzsi output for loop.

--- Program.cs
+++ Program.cs
@@ -12,3 +12,11 @@
 string name = "Peter";
 
 Console.WriteLine($"Hello {name}, my favofir veggie is {vegetables[Random.Shared.Next(vegetables.Length)]}");
+
+
+//> BitMiller:
+for (int i = 0; i < vegetables.Count(); i++)
+{
+    Console.WriteLine($"Vedzsi {Math.Floor(Convert.ToDouble(i/10))}{i%10}.: {vegetables[i]}");
+}
+
